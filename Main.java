a19c5a6470efd58d0c9509e23bc7ed1525b65e26
Maxwell Schultz <schultm8@callisto.vc.restek.wwu.edu>
2022-04-20 14:26:53 -0700

    changed main so that input doesnt rly on cmd line args, also test.txt for input

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 	/* Entry point of the application */
     public static void main(String[] args) throws FileNotFoundException {
         LexicalAnalyzer surly = new LexicalAnalyzer();
-        surly.run(args[0]);
+        String argZero = "./test.txt";
+        surly.run(argZero);
     }
 }
